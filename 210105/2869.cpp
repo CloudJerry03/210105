9c3dd7728c101c881658f954204ebf1f3f3480ac
DESKTOP-P8RTASM\Cloud <gomdalp1701@gmail.com>
2021-01-06 00:29:55 +0900

    2869 달팽이 올려보냄

---
+++ 210105/2869.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A, B, V;
+
+	cin >> A >> B >> V;
+
+
+	if ((V-A) % (A - B) == 0)
+		cout << ((V - A) / (A - B)) + 1 << endl;
+	else
+		cout << ((V - A) / (A - B)) + 2 << endl;
+}
